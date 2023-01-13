809512f7a2bdff86ead651ca49ca7f5e945e1a54
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-01-13 13:31:47 +0300

    сделан блок new

--- js/slider.js
+++ js/slider.js
@@ -2,5 +2,33 @@ var swiper1 = new Swiper(".advertisement-slider", {
   loop: true,
   pagination: {
     el: ".advertisement-pagination",
+    clickable: true,
+  },
+});
+
+var swiper3 = new Swiper(".new-slider", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 45,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+    draggable: true,
+  },
+  navigation: {
+    nextEl: ".new-next",
+    prevEl: ".new-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    767: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
 });
