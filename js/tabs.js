9eba72cc31abeb0063cb893c7607bbdcb3370db3
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-01-26 13:44:26 +0300

    исправлен блок популярных товаров

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,10 @@
 const tabNavs = document.querySelectorAll(".nav-tab");
 const tabPanes = document.querySelectorAll(".tab-pane");
 
-for (var i = 0; i < tabNavs.length; i++) {
+for (let i = 0; i < tabNavs.length; i++) {
   tabNavs[i].addEventListener("click", function (e) {
     e.preventDefault();
-    var activeTabAttr = e.target.getAttribute("data-tab");
+    const activeTabAttr = e.target.getAttribute("data-tab");
 
     for (let j = 0; j < tabNavs.length; j++) {
       const contentAttr = tabPanes[j].getAttribute("data-tab-content");
@@ -15,6 +15,28 @@ for (var i = 0; i < tabNavs.length; i++) {
       } else {
         tabNavs[j].classList.remove("active");
         tabPanes[j].classList.remove("active");
+      }
+    }
+  });
+}
+
+const popularTab = document.querySelectorAll(".popular-tab");
+const tabPopular = document.querySelectorAll(".tab-popular");
+
+for (var i = 0; i < popularTab.length; i++) {
+  popularTab[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    let activeTabAttr = e.target.getAttribute("pop-tab");
+
+    for (let j = 0; j < popularTab.length; j++) {
+      const contentAttr = tabPopular[j].getAttribute("popular-tab-content");
+
+      if (activeTabAttr === contentAttr) {
+        popularTab[j].classList.add("active");
+        tabPopular[j].classList.add("active");
+      } else {
+        popularTab[j].classList.remove("active");
+        tabPopular[j].classList.remove("active");
       }
     }
   });
