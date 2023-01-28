1e95a0455f5dbf638ef61d4aebc3fdb8038727c6
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-01-28 11:16:08 +0300

    исправлен папап

--- js/index.js
+++ js/index.js
@@ -1,19 +1,22 @@
 const scrollTop = document.querySelector(".footer-scroll-top");
 const burgerButton = document.querySelector(".header-burger");
 const burgerPopup = document.querySelector(".popup");
+const header = document.querySelector(".header");
+
 const navList = document.querySelector("#nav-list").cloneNode(1);
 const aboutList = document.querySelector("#header-about").cloneNode(1);
 const body = document.body;
 
-burgerButton.addEventListener("click", hambHandler);
+burgerButton.addEventListener("click", OpenModal);
 
 scrollTop.addEventListener("click", () => {
   window.scrollTo(pageYOffset, 0);
 });
 
-function hambHandler(e) {
+function OpenModal(e) {
   e.preventDefault();
   burgerPopup.classList.toggle("open");
+  body.classList.toggle("noscroll");
   renderPopup();
 }
 
