92f07cefc1d101e1030bfedec659512374c36fc5
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-02-02 12:03:42 +0300

    страница каталога: исправлен гланый блок

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const burgerButton = document.querySelector(".header-burger");
 const burgerPopup = document.querySelector(".popup");
 const header = document.querySelector(".header");
-
 const navList = document.querySelector("#nav-list").cloneNode(1);
 const aboutList = document.querySelector("#header-about").cloneNode(1);
+const scrollTop = document.querySelector(".footer-scroll-top");
 const body = document.body;
-
 burgerButton.addEventListener("click", OpenModal);
 
 function OpenModal(e) {
@@ -20,3 +19,7 @@ function renderPopup() {
   burgerPopup.appendChild(aboutList);
   burgerPopup.appendChild(navList);
 }
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo(pageYOffset, 0);
+});
--- js/scroll.js
+++
@@ -1,5 +0,0 @@
-const scrollTop = document.querySelector(".footer-scroll-top");
-
-scrollTop.addEventListener("click", () => {
-  window.scrollTo(pageYOffset, 0);
-});
